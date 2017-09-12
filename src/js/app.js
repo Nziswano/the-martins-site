c961aad8103fb79e168bf1df7402822ee1a09da4
Johan Martin <martin.johan@johan-martin.com>
2017-09-12 23:24:10 +0200

    update apps with latest

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,9 @@
-import $ from 'jquery';
-import whatInput from 'what-input';
+import $ from 'jquery'
+import whatInput from 'what-input'
 import './lib/foundation-explicit-pieces'
 
 import '../scss/app.scss'
 
 window.$ = $
-
-// import Foundation from 'foundation-sites';
-// // If you want to pick and choose which modules to include, comment out the above and uncomment
-// // the line below
-// //import './lib/foundation-explicit-pieces';
-
-
 
 $(document).foundation()
